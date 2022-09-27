ff5393d007bccb6820183a00080a24d8f106d290
MD Sanju <mdsanju387@gmail.com>
2022-09-27 07:11:04 +0600

    EJS Dynamic data passrd

--- index.js
+++ index.js
@@ -2,12 +2,22 @@ const express = require('express')
 const app = express()
 const port = 5000
 
-// EJS Setup
-app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.render('index', {})
-})
+// EJS Setup
+app.set("view engine", "ejs");
+
+let prommingLanguages = [];
+
+app.get("/", (req, res) => {
+  res.render("index", { pLNames: prommingLanguages });
+});
+
+app.post("/", (req, res) => {
+  const programmingLanguage = req.body.programmingLanguage;
+  prommingLanguages.push(programmingLanguage);
+  res.redirect("/");
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
